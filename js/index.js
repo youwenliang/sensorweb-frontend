260b136fc08d45b66ca7763a747fe4546ebb78d5
Mark Liang <big.head.cool@gmail.com>
2016-03-16 17:55:57 +0800

    Add url & scroll

--- js/index.js
+++ js/index.js
@@ -2,3 +2,14 @@
   $('.button-collapse').sideNav();
   $('.parallax').parallax();
 })();
+
+
+$('#learn').click(function(e){
+	e.preventDefault();
+	$('html, body').animate({
+        scrollTop: $("#what").offset().top-64,
+        easing: "easeinout"
+    }, 700);
+    return false;
+});
+    
